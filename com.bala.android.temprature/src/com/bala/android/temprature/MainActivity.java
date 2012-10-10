aa3706ba064c67dbb84eb07e2e591891e3807f7c
ruwan <ruwan@myServer>
2012-10-10 09:40:48 +0530

    few chnages

--- com.bala.android.temprature/src/com/bala/android/temprature/MainActivity.java
+++ com.bala.android.temprature/src/com/bala/android/temprature/MainActivity.java
@@ -2,20 +2,24 @@ package com.bala.android.temprature;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private EditText mEditText;
+	private TextView mViewText;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		mEditText = (EditText) findViewById(R.id.editText1);
+		mViewText = (TextView) findViewById(R.id.textView1);
 	}
 
 	@Override
@@ -25,36 +29,39 @@ public class MainActivity extends Activity {
 	}
 
 	public void calculate(View view) {
+		Log.d("MYINFO", "Click on button");
+		if (mEditText.getText().length() == 0) {
+			Toast.makeText(this, "Please enter a valid number",
+					Toast.LENGTH_LONG).show();
+			return;
+		}
+		float inputValue = Float.parseFloat(mEditText.getText().toString());
 		switch (view.getId()) {
-		case R.id.button1:
+		case R.id.radio0:
+			Log.d("MYINFO", "I am in Celsius");
 			RadioButton inCelsius = (RadioButton) findViewById(R.id.radio0);
-			RadioButton inFahrenheit = (RadioButton) findViewById(R.id.radio1);
-			if (mEditText.getText().length() == 0) {
-				Toast.makeText(this, "Please enter a valid number",
-						Toast.LENGTH_LONG).show();
-				return;
-			}
-			float inputValue = Float.parseFloat(mEditText.getText().toString());
 			if (inCelsius.isChecked()) {
-				mEditText.setText(String
+				mViewText.setText(String
 						.valueOf(convertFahrenheitToCelsius(inputValue)));
-				inCelsius.setChecked(false);
-				inFahrenheit.setChecked(true);
-			} else {
-				mEditText.setText(String
-						.valueOf(convertCelsiusToFahrenheit(inputValue)));
-				inFahrenheit.setChecked(false);
-				inCelsius.setChecked(true);
+				//inCelsius.setChecked(false);
+				//inFahrenheit.setChecked(true);
 			}
 			break;
+		case R.id.radio1:
+			Log.d("MYINFO", "I am in Fahrenheit");
+			RadioButton inFahrenheit = (RadioButton) findViewById(R.id.radio1);
+			if (inFahrenheit.isChecked()) {
+				mViewText.setText(String
+						.valueOf(convertCelsiusToFahrenheit(inputValue)));
+			}
 		}
 	}
 	
 	private float convertCelsiusToFahrenheit(float val){
-		return val;
+		return ((val - 32) * 5 / 9);
 	}
 	
 	private float convertFahrenheitToCelsius(float val){
-		return val;
+		return ((val * 9) / 5) + 32;
 	}
 }
